2737823ad9f444cb7a7b91b67ba7dbb34e64acee
JDems <128564706+JDems@users.noreply.github.com>
2023-09-02 20:07:27 -0700

    Add files via upload

---
+++ Starter_Code/starter_code/api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "023210bb0787f34737a94e3ed97ea9bd"
+
+#0a36f6df693d12cc29969feb7601203d
+#a113ddb8ba3030f1adff5235c8100db0
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
